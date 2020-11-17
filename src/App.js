c59bbdbf59e9dfa341594af9aa0f9bde742613dd
BitsHeart <ga_bennecer@esi.dz>
2020-11-17 10:29:00 +0100

    cleanup : make some changes on appJs 💯

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
     <div className="App">
+      <div className="app__header">
+        <img 
+          className="app__headerImage" 
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+          alt="instagram logo"
+        />
+        <input type="text" placeholder="Rechercher"/>
+        <h4>Sign Up Sign In</h4>
+      </div>
       <h1>Hello Zaki</h1>
+
+      <Post />
+      {/* Posts */}
+      {/* Posts */}
+      {/* Posts */}
+
     </div>
   );
 }
